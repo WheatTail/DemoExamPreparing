0a0e1157a543dd91f23ffde52f6398cd652a2518
Wheat Tail <hartovsky@gmail.com>
2019-02-18 19:15:24 +0300

    И ещё формы. 13/36.

---
+++ DemoExamPreparing/MS_CharitiesList.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DemoExamPreparing
+{
+    partial class MS_CharitiesList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.GoBackButton = new System.Windows.Forms.Button();
+            this.GrayBackgroundLabel = new System.Windows.Forms.Label();
+            this.FooterLabel = new System.Windows.Forms.Label();
+            this.GratBackgroundFooterLabel = new System.Windows.Forms.Label();
+            this.CharityListHeaderLabel = new System.Windows.Forms.Label();
+            this.CharityListTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.HeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HeaderLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HeaderLabel.Location = new System.Drawing.Point(93, 17);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(170, 15);
+            this.HeaderLabel.TabIndex = 20;
+            this.HeaderLabel.Text = "MARATHON SKILLS 2019";
+            // 
+            // GoBackButton
+            // 
+            this.GoBackButton.Location = new System.Drawing.Point(12, 12);
+            this.GoBackButton.Name = "GoBackButton";
+            this.GoBackButton.Size = new System.Drawing.Size(75, 23);
+            this.GoBackButton.TabIndex = 19;
+            this.GoBackButton.Text = "Назад";
+            this.GoBackButton.UseVisualStyleBackColor = true;
+            // 
+            // GrayBackgroundLabel
+            // 
+            this.GrayBackgroundLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.GrayBackgroundLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GrayBackgroundLabel.Location = new System.Drawing.Point(0, 0);
+            this.GrayBackgroundLabel.Name = "GrayBackgroundLabel";
+            this.GrayBackgroundLabel.Size = new System.Drawing.Size(484, 48);
+            this.GrayBackgroundLabel.TabIndex = 18;
+            // 
+            // FooterLabel
+            // 
+            this.FooterLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.FooterLabel.AutoSize = true;
+            this.FooterLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FooterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FooterLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FooterLabel.Location = new System.Drawing.Point(127, 335);
+            this.FooterLabel.Name = "FooterLabel";
+            this.FooterLabel.Size = new System.Drawing.Size(249, 17);
+            this.FooterLabel.TabIndex = 28;
+            this.FooterLabel.Text = "Сколько-то дней до чего-то там";
+            // 
+            // GratBackgroundFooterLabel
+            // 
+            this.GratBackgroundFooterLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.GratBackgroundFooterLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GratBackgroundFooterLabel.Location = new System.Drawing.Point(0, 325);
+            this.GratBackgroundFooterLabel.Name = "GratBackgroundFooterLabel";
+            this.GratBackgroundFooterLabel.Size = new System.Drawing.Size(484, 36);
+            this.GratBackgroundFooterLabel.TabIndex = 27;
+            // 
+            // CharityListHeaderLabel
+            // 
+            this.CharityListHeaderLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CharityListHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CharityListHeaderLabel.Location = new System.Drawing.Point(0, 48);
+            this.CharityListHeaderLabel.Name = "CharityListHeaderLabel";
+            this.CharityListHeaderLabel.Size = new System.Drawing.Size(484, 86);
+            this.CharityListHeaderLabel.TabIndex = 29;
+            this.CharityListHeaderLabel.Text = "Список благотворительных организаций\r\n\r\nЭто - список всех благотворительных учреж" +
+    "дений, которые поддерживаются в Marathon Skills 2019. Спасибо за помощь вы подде" +
+    "рживаете их, спонсируя бегунов!\r\n";
+            this.CharityListHeaderLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CharityListTableLayoutPanel
+            // 
+            this.CharityListTableLayoutPanel.ColumnCount = 2;
+            this.CharityListTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.CharityListTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.CharityListTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CharityListTableLayoutPanel.Location = new System.Drawing.Point(0, 134);
+            this.CharityListTableLayoutPanel.Name = "CharityListTableLayoutPanel";
+            this.CharityListTableLayoutPanel.RowCount = 2;
+            this.CharityListTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.CharityListTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.CharityListTableLayoutPanel.Size = new System.Drawing.Size(484, 191);
+            this.CharityListTableLayoutPanel.TabIndex = 30;
+            // 
+            // MS_CharitiesList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.CharityListTableLayoutPanel);
+            this.Controls.Add(this.CharityListHeaderLabel);
+            this.Controls.Add(this.FooterLabel);
+            this.Controls.Add(this.GratBackgroundFooterLabel);
+            this.Controls.Add(this.HeaderLabel);
+            this.Controls.Add(this.GoBackButton);
+            this.Controls.Add(this.GrayBackgroundLabel);
+            this.Name = "MS_CharitiesList";
+            this.Text = "MS_CharitiesList";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Button GoBackButton;
+        private System.Windows.Forms.Label GrayBackgroundLabel;
+        private System.Windows.Forms.Label FooterLabel;
+        private System.Windows.Forms.Label GratBackgroundFooterLabel;
+        private System.Windows.Forms.Label CharityListHeaderLabel;
+        private System.Windows.Forms.TableLayoutPanel CharityListTableLayoutPanel;
+    }
+}
---
+++ DemoExamPreparing/MS_InteractiveMap.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DemoExamPreparing
+{
+    partial class MS_InteractiveMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MS_InteractiveMap));
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.GoBackButton = new System.Windows.Forms.Button();
+            this.GrayBackgroundLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.InteractiveMapHeaderLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.HeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HeaderLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HeaderLabel.Location = new System.Drawing.Point(93, 17);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(295, 15);
+            this.HeaderLabel.TabIndex = 20;
+            this.HeaderLabel.Text = "Интерактивная карта Marathon Skills 2019";
+            // 
+            // GoBackButton
+            // 
+            this.GoBackButton.Location = new System.Drawing.Point(12, 12);
+            this.GoBackButton.Name = "GoBackButton";
+            this.GoBackButton.Size = new System.Drawing.Size(75, 23);
+            this.GoBackButton.TabIndex = 19;
+            this.GoBackButton.Text = "Назад";
+            this.GoBackButton.UseVisualStyleBackColor = true;
+            // 
+            // GrayBackgroundLabel
+            // 
+            this.GrayBackgroundLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.GrayBackgroundLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GrayBackgroundLabel.Location = new System.Drawing.Point(0, 0);
+            this.GrayBackgroundLabel.Name = "GrayBackgroundLabel";
+            this.GrayBackgroundLabel.Size = new System.Drawing.Size(484, 48);
+            this.GrayBackgroundLabel.TabIndex = 18;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InteractiveMapHeaderLabel
+            // 
+            this.InteractiveMapHeaderLabel.AutoSize = true;
+            this.InteractiveMapHeaderLabel.Location = new System.Drawing.Point(298, 51);
+            this.InteractiveMapHeaderLabel.Name = "InteractiveMapHeaderLabel";
+            this.InteractiveMapHeaderLabel.Size = new System.Drawing.Size(35, 13);
+            this.InteractiveMapHeaderLabel.TabIndex = 22;
+            this.InteractiveMapHeaderLabel.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(424, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(366, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(407, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "label7";
+            // 
+            // MS_InteractiveMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InteractiveMapHeaderLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.HeaderLabel);
+            this.Controls.Add(this.GoBackButton);
+            this.Controls.Add(this.GrayBackgroundLabel);
+            this.Name = "MS_InteractiveMap";
+            this.Text = "MS_InteractiveMap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Button GoBackButton;
+        private System.Windows.Forms.Label GrayBackgroundLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label InteractiveMapHeaderLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
